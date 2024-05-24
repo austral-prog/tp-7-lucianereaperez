fb398abe47dd7daadfdb53f0cd304412d7886667
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-05-23 22:01:39 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,22 @@
 def enumerate_list(list):
-    return list
+    new_list = []
+    new_new_list = []
+    for element in list:
+        if len(element) != 0 :
+            new_list.append(element)
+    for index, element2 in enumerate(new_list):
+        espacio = f"{int(index)}. {element2.title()}"
+        new_new_list.append(espacio)
+    return new_new_list
 
 
 def enumerate_backwards(list):
-    return list
+    new_listback = []
+    new_new_listback = []
+    for element in list:
+        if len(element) != 0 :
+            new_listback.append(element)
+    for index, element2 in enumerate(new_listback):
+        espacio = f"{int(index)}. {element2.title()[::-1]}"
+        new_new_listback.append(espacio)
+    return new_new_listback

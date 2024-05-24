da35f3d3b02e689a5fc5ad28afb64ac1b3a18744
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-05-23 23:01:54 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,46 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, my_list, index):
+    i=index
+    while i < len(my_list):
+        if my_list[i]==word:
+            return i
+        i+=1
     return -1
 
 
-def index_of_empty(list):
+def index_of_empty(my_list):
+    i=0
+    while i < len(my_list):
+        if my_list[i] == "":
+            return i
+        i += 1
     return -1
 
 
-def index_of(word, list):
+def index_of(word, my_list):
+    i=0
+    while i < len(my_list):
+        if my_list[i]==word:
+            return i 
+        i+=1
     return -1
 
 
-def put(word, list):
+def put(word, my_list):
+    i=0
+    while i < len(my_list):
+        if my_list[i]=="":
+            my_list[i]= word
+            return i
+        i+=1
     return -1
 
 
-def remove(word, list):
-    return -1
+def remove(word, my_list):
+    i=0
+    count=0
+    while i < len(my_list):
+        if my_list[i]==word:
+            my_list[i]=""
+            count+=1
+        i+=1
+    return count
